0893c882df6e851b5a5919542eec67e9c92c3fd3
Carla Stickler <carlastickler@gmail.com>
2019-01-31 23:06:14 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let tracker = 0 
+
+const checkKeyCode = () => {
+  //we need to know where we are in the sequence
+  const whatTheyPressed = event.keyCode 
+  if (whatTheyPressed === code[tracker]) {
+    tracker += 1
+  } else {
+    tracker = 0 
+  }
+  console.log(tracker)
+}
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+document.addEventListener('keydown', checkKeyCode)
 }
+
+init()
